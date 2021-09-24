7c616aaa52eb42998abe70b5f06e592bb7994050
codeNoob2281 <91195909+codeNoob2281@users.noreply.github.com>
2021-09-24 13:55:32 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 // 读取json文件
-function readJSONFile(dataPath,callback){
-   var request=new XMLHttpRequest();
-   request.open("get",dataPath);
-   request.send("null");
-   request.onload=function (){
-         if(request.status==200){
-             var json=JSON.parse(request.responseText);
-             callback(null,json);
-         }
-         else {
-             callback("读取失败",null);
+function readJSONFile(dataPath, callback) {
+    var request = new XMLHttpRequest();
+    request.open("get", dataPath);
+    request.send("null");
+    request.onload = function () {
+        if (request.status == 200) {
+            var json = JSON.parse(request.responseText);
+            callback(null, json);
+        } else {
+            callback("读取失败", null);
 
-         }
-   }
+        }
+    }
+}
+
+
+window.onload = function () {
+
+
 }
