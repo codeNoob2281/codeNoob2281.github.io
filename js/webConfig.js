cb67abb881bedd5e8e7d5c448b64ef3dd2078bb4
codeNoob2281 <91195909+codeNoob2281@users.noreply.github.com>
2021-09-27 19:51:05 +0800

    Update webConfig.js

--- js/webConfig.js
+++ js/webConfig.js
@@ -1,6 +1,6 @@
 //是否部署在服务器
-const isOnServer=false;
+const isOnServer=true;
 //是否使用github pages服务
-const isOnGithub=false;
+const isOnGithub=true;
 //是否使用gitee pages服务
-const isOnGitee=true;
+const isOnGitee=false;
